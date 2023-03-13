37e5f67de8e3b290e2e1ab9653c2d64380b541a3
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2023-03-13 15:41:28 +0200

    itemdatabase

---
+++ src/pages/itemDatabase.js
@@ -0,0 +1,22 @@
+export const items = [
+    {   
+        img: 'https://cdn.pixabay.com/photo/2021/09/26/20/49/world-6658881_960_720.jpg',
+        title: 'Item 1',        
+        description: 'Description here',
+        id: 0,        
+    },
+
+    {   
+        img: 'https://cdn.pixabay.com/photo/2022/04/15/19/54/flowers-7135053_960_720.jpg',
+        title: 'Item 2',        
+        description: 'Description here',
+        id: 1,        
+    },
+
+    {   
+        img: 'https://cdn.pixabay.com/photo/2022/01/31/12/46/bird-6983434_960_720.jpg',
+        title: 'Item 3',        
+        description: 'Description here',
+        id: 2,        
+    },    
+]
