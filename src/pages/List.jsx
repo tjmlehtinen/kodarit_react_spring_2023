545e3bf3608cabfe4817c29de3bd1c8149928f84
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2023-03-27 19:30:09 +0300

    list removea vaille valmis

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -2,16 +2,50 @@ import React from "react";
 import { useState } from "react";
 
 export const List = () => {
+    const [content, setContent] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [itemId, setItemId] = useState(0);
+
     return (
-        <form>
-            <input
-                type="text"
-                placeholder="type something..."
-            />
-            <input
-                type="submit"
-                value="Add this!"
-            />
-        </form>
+        <div>
+            <form
+                onSubmit={
+                    (event) => {
+                        event.preventDefault()
+                        const newItem = {data: inputValue, id: itemId}
+                        setInputValue("")
+                        setItemId(itemId + 1)
+                        const newContent = [newItem, ...content]
+                        setContent(newContent)
+                    }
+                }
+            >
+                <input
+                    type="text"
+                    placeholder="type something..."
+                    value={inputValue}
+                    onChange={(event) => setInputValue(event.target.value)}
+                    required
+                />
+                <input
+                    type="submit"
+                    value="Add this!"
+                />
+            </form>
+            <ul>
+                {
+                    content.map((item) =>
+                        <li key={item.id}>
+                            {item.data}
+                            <button
+                                type="button"
+                            >
+                                Remove
+                            </button>
+                        </li>
+                    )
+                }
+            </ul>
+        </div>
     );
 }
