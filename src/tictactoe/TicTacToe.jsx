c5af1ed0739a46ac3b9c4a4d164711b3da6fefbd
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2023-04-24 19:28:52 +0300

    pelin tila ja klikkailemalla muutos

--- src/tictactoe/TicTacToe.jsx
+++ src/tictactoe/TicTacToe.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { useState } from "react";
+
 import { Board } from "./Board";
 
 import "./TicTacToe.css";
 
+const initialBoard = ["", "", "", "", "", "", "", "", ""];
+
 export const TicTacToe = () => {
+    const [gameState, setGameState] = useState(initialBoard);
+    const [isXTurn, setIsXTurn] = useState(true);
+
+    function onSquareClick(index) {
+        if (gameState[index] !== "") {
+            return;
+        }
+        let newGameState = gameState.slice();
+        newGameState[index] = isXTurn ? "X" : "O";
+        setGameState(newGameState);
+        setIsXTurn(!isXTurn);
+    }
+
     return (
         <div className="game">
             <h1>Tic-Tac-Toe</h1>
-            <Board />
+            <Board
+                gameState={gameState}
+                onSquareClick={onSquareClick}
+            />
         </div>
-    )
+    );
 }
